866d4b1f01ec3b40e5945a11772a6da96d41451b
Nicolas Murillo <uninoma_20@hotmail.com>
2021-09-15 00:10:45 -0500

    Deplegando proyecto

---
+++ pr_tecnica/lists.py
@@ -0,0 +1,21 @@
+
+cargos_list = [
+    ('Ejecutivo_Comercial','Ejecutivo Comercial'),
+    ('Gerente_Comercial','Gerente Comercial'),
+    ('Subgerente_Comercial','Subgerente Comercial'),
+    ('Gerente_Regional','Gerente Regional'),
+    ('Asistente_Gerencia','Asistente_Gerencia'),
+]
+
+zonas_list = [
+    ('1','Zona 1'),
+    ('2','Zona 2'),
+    ('3','Zona 3'),
+    ('4','Zona 4'),
+    ('5','Zona 5')
+]
+
+sexos_list = [
+    ('F','Mujer'),
+    ('M','HOmbre')
+]
