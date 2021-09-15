7f357b794a2efe81a88a23149cbf8b405ad761b7
Nicolas Murillo <uninoma_20@hotmail.com>
2021-09-15 13:59:32 -0500

    Cambios en usuario

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from apps.usuario.views import index,detail,next
+from apps.usuario.views import index,detail,create,next
 from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('', login_required(index),name='index'),
     path('detail/<id_usuario>/', login_required(detail),name='detail'),
+    path('create/', login_required(create),name='create'),
     path('next/<usuario>/', login_required(next),name='next'),
 ]
--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from apps.usuario.forms import UsuarioForm
 from apps.usuario.models import Usuario
+from apps.venta.models import Venta
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -32,7 +33,14 @@ def index(request):
 @login_required
 def detail(request,id_usuario):
     obj = Usuario.objects.get(id=id_usuario)
-    contexto = {'usuario':obj}
+    if obj.cargo == 'Ejecutivo_Comercial' or None:
+        rel_objs = Venta.objects.filter(vendedor=obj)
+        flag = False
+    else:
+        rel_objs = Usuario.objects.filter(jefe=obj)
+        flag = True
+    print(rel_objs)
+    contexto = {'usuario':obj,'rel_objs':rel_objs,'flag':flag}
     return render(request,'usuario/detail.html', contexto)
 
 
