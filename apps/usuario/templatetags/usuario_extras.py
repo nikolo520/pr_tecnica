92be20dd45ae252f77a98e496deb644c34555074
Nicolas Murillo <uninoma_20@hotmail.com>
2021-09-15 23:31:30 -0500

    Filtro Total Ventas

---
+++ apps/usuario/templatetags/usuario_extras.py
@@ -0,0 +1,19 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='total_ventas')
+def total_ventas(vendedor_id):
+    from apps.venta.models import Venta
+    try:
+
+        ventas = Venta.objects.filter(vendedor_id = vendedor_id)
+        total = 0
+        
+        for venta in ventas:
+            if venta.valor > 0:
+                total += venta.valor
+    except Exception as e:
+        print(e)
+        total = 0
+    return total
