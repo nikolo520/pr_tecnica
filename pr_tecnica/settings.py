25eeae69e1b6c55d4995a820a122650e11a0e090
Nicolas Murillo <uninoma_20@hotmail.com>
2021-09-15 23:18:50 -0500

    Separando staticos

--- pr_tecnica/settings.py
+++ pr_tecnica/settings.py
@@ -118,13 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static_files'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
