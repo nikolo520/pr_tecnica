af036615062a11a9fe5a7059986875a816592da6
Nicolas Murillo <uninoma_20@hotmail.com>
2021-09-16 20:00:23 -0500

    Demo

---
+++ pr_tecnica/config.py
@@ -0,0 +1,13 @@
+DEBUG_CONF = True
+
+# DB_CONF = {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'NAME': 'test',
+#             'USER':'root',
+#             'PASSWORD':'',
+#             'HOST':'127.0.0.1'
+#         }
+DB_CONF = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
